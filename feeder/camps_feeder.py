3b7fdeb979b098d6b3e824dda3c1668e2769f54e
Ajitsen Surendran <ajitsen_s@yahoo.co.in>
2018-08-21 23:31:43 +0530

    adding data for supply

--- feeder/camps_feeder.py
+++ feeder/camps_feeder.py
@@ -3,7 +3,7 @@ from feeder.common.config import CAMP_SHEET, RESOURCE_SHEET, VOLUNTEER_SHEET
 from feeder.common.solr_helper import CAMP_COLLECTION, feed_csv_to_solr, VOLUNTEER_COLLECTION, RESOURCE_COLLECTION, \
     get_data_frame_from_json
 from feeder.common.utils import save_data_json
-from feeder.resource_transformer import process_suply_feed
+from feeder.resource_transformer import process_supply_feed
 from feeder.volunteer_transformer import process_volunteer_feed
 
 
@@ -18,12 +18,12 @@ def process(sheet_url, collection):
         data_frame = get_data_frame_from_json(json_file, must_fields=['mobileNumber'])
         process_volunteer_feed(data_frame, csv_file)
     else:
-        data_frame = get_data_frame_from_json(json_file, must_fields=['contactNo'])
-        process_suply_feed(data_frame, csv_file)
+        data_frame = get_data_frame_from_json(json_file, must_fields=['contactDetails'])
+        process_supply_feed(data_frame, csv_file)
     feed_csv_to_solr(collection, csv_file)
 
 
 # MAIN
-#process(CAMP_SHEET, CAMP_COLLECTION)
+process(CAMP_SHEET, CAMP_COLLECTION)
 process(VOLUNTEER_SHEET, VOLUNTEER_COLLECTION)
-#process(RESOURCE_SHEET, RESOURCE_COLLECTION)
+process(RESOURCE_SHEET, RESOURCE_COLLECTION)
